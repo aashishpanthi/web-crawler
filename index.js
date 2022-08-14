941370fcf48073cde030bc7342685ae880a159a7
Aashish Panthi <aashishpanthi11@gmail.com>
2022-08-14 09:42:15 +0545

    no-sandbox arg added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    defaultViewport: {
+      width: 1920,
+      height: 1080,
+    },
+    args: ["--no-sandbox", "--headless"],
+  });
   const page = await browser.newPage();
   await page.goto("https://example.com");
-  await page.screenshot({ path: "example.png" });
+  await page.screenshot({ path: "test.png" });
 
   await browser.close();
 })();
