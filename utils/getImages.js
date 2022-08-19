6bdb4dc341043b0804f338b2bb560baa97fdc7b7
Aashish Panthi <aashishpanthi11@gmail.com>
2022-08-19 16:32:38 +0545

    check the background image

--- utils/getImages.js
+++ utils/getImages.js
@@ -1,7 +1,11 @@
 // Get all images from a webpage with alt text
-async function getImages(page, browser) {
+async function getImages(page) {
   const images = await page.evaluate(() => {
+    // Get all images from the page with img tag
     const images = Array.from(document.querySelectorAll("img"));
+
+    console.log("images");
+
     return images.map((image) => {
       return {
         src: image.src,
@@ -9,7 +13,26 @@ async function getImages(page, browser) {
       };
     });
   });
-  return images;
+
+  // Get all images with from the page set as background image
+  const backgroundImages = await page.evaluate(() => {
+    const backgroundImages = Array.from(document.querySelectorAll("*"));
+
+    return backgroundImages
+      .map((backgroundImage) => {
+        return {
+          src: backgroundImage.style.backgroundImage
+            .replace(/^url\(['"]?/, "")
+            .replace(/['"]?\)$/, ""),
+          alt: backgroundImage.alt,
+        };
+      })
+      .filter((backgroundImage) => {
+        return backgroundImage.src !== "";
+      });
+  });
+
+  return [...images, ...backgroundImages];
 }
 
 export default getImages;
