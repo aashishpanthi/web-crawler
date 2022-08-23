b237178718130f78d785a11d44c24bd7a7070ae3
Aashish Panthi <aashishpanthi11@gmail.com>
2022-08-23 12:27:41 +0545

    fetch first few lines of body

--- utils/scrapeContent/getFirstFewLines.js
+++ utils/scrapeContent/getFirstFewLines.js
@@ -1,31 +1,12 @@
 // Get the first few lines after the heading tags
 
 const getFirstFewLines = async (page) => {
-  return page
-    .evaluate(() => {
-      const headings = Array.from(
-        document.querySelectorAll("h1, h2, h3, h4, h5, h6")
-      );
-      const firstFewLines = [];
-      let currentLine = 0;
-      let currentHeading = 0;
-      while (currentLine < 10 && currentHeading < headings.length) {
-        const heading = headings[currentHeading];
-        const headingPosition = heading.getBoundingClientRect().top;
-        if (headingPosition > currentLine) {
-          firstFewLines.push(heading.textContent);
-          currentLine = headingPosition;
-        }
-        currentHeading++;
-      }
-      return firstFewLines;
-    })
-    .then((firstFewLines) => {
-      return firstFewLines;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // get the first few lines of a body tag
+  const body = await page.$("body");
+  const bodyText = await body.evaluate((body) =>
+    body.innerText.split("\n").slice(10, 60).join("\n")
+  );
+  return bodyText;
 };
 
 export default getFirstFewLines;
